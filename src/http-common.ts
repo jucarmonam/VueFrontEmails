596e0eaaeac5e393e7eca90be0a242daf45d86d0
Juan Pablo <jucarmonam@unal.edu.co>
2023-08-08 22:39:48 -0500

    update url backend https

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://18.224.69.79:8080',
+  baseURL: 'https://18.224.69.79:8080',
   headers: {
     'Content-type': 'application/json'
   }
